2f34c58bcee30931ae6694e0327013096f62588c
Ji-0 <xaonoax@gmail.com>
2024-02-07 19:06:27 +0900

    [Spring] 스프링 핵심 원리 - 기본편 6(컴포넌트 스캔) - 중복 등록과 충돌 - 수동 빈 등록과 자동 빈 등록에서 빈 이름이 충돌될 경우

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,5 +12,11 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)  // @ComponentScan이 스캔하는 과정에서 특정 클래스나 어노테이션을 제외하도록 필터링한다.
 )
 public class AutoAppConfig {
+
+    // 수동 빈 등록과 자동 빈 등록에서 빈 이름이 충돌될 경우
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
